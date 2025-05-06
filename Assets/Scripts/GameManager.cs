c5099869bcb0a78dac689ea8070c83aaae977373
Engincan Danabaş <60680749+engincandanabas@users.noreply.github.com>
2025-05-06 13:16:05 +0300

    Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class GameManager : NetworkBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public event EventHandler OnGameStarted;
+    
+
+    public enum PlayerType
+    {
+        None,
+        PlayerLeft,
+        PlayerRight
+    }
+    public PlayerType localPlayerType;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log("Network spawned : " + NetworkManager.Singleton.LocalClientId);
+        if (NetworkManager.Singleton.LocalClientId == 0)
+        {
+            localPlayerType = PlayerType.PlayerLeft;
+        }
+        else
+        {
+            localPlayerType = PlayerType.PlayerRight;
+        }
+        if (IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += NetworkManager_OnClientConnectedCallback;
+        }
+    }
+
+    private void NetworkManager_OnClientConnectedCallback(ulong obj)
+    {
+        if (NetworkManager.Singleton.ConnectedClients.Count == 2)
+        {
+            // Start Game
+            TriggerOnGameStartedRpc();
+        }
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void TriggerOnGameStartedRpc()
+    {
+        OnGameStarted?.Invoke(this, null);
+    }
+    public PlayerType GetLocalPlayerType()
+    {
+        return localPlayerType;
+    }
+}
