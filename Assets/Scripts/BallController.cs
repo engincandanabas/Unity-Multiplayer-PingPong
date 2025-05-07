a560d81befa54ec9140d570b680072aa1625fdad
Engincan Danabaş <60680749+engincandanabas@users.noreply.github.com>
2025-05-07 18:14:39 +0300

    Score Event

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,26 +1,66 @@
+using System;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    public event EventHandler<OnPlayerScoreArgs> OnPlayerScore;
+    public class OnPlayerScoreArgs : EventArgs
+    {
+        public GameManager.PlayerType scorePlayerType;
+    }
+
     [SerializeField] private float movementSpeed;
     [SerializeField] private float maxInitialAngle=0.67f;
+    [SerializeField] private float velocityMultiplier=1.1f;
     private Rigidbody2D rb;
     private Vector2 direction;
+
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        InitialPush();
+        GameManager.Instance.OnGameStarted += GameManager_OnGameStarted;
+        OnPlayerScore += ResetBall;
+        OnPlayerScore += InitialPush;
+    }
+    private void OnDisable()
+    {
+        GameManager.Instance.OnGameStarted -= GameManager_OnGameStarted;
+        OnPlayerScore -= ResetBall;
+        OnPlayerScore -= InitialPush;
+    }
+    private void GameManager_OnGameStarted(object sender, EventArgs eventArgs)
+    {
+        
     }
 
-    
-    private void InitialPush()
+    private void InitialPush(object sender,OnPlayerScoreArgs onPlayerScoreArgs)
     {
         Vector2 dir = Vector2.left;
-        dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
+        dir.y = UnityEngine.Random.Range(-maxInitialAngle, maxInitialAngle);
         rb.linearVelocity = dir * movementSpeed;
+    }
+    private void ResetBall(object sender, OnPlayerScoreArgs onPlayerScoreArgs)
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        rb.linearVelocity = Vector2.zero;
+        this.transform.position = Vector2.zero;
+        GetComponent<SpriteRenderer>().enabled = true;
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.gameObject.name);
+        if(collision.gameObject.CompareTag("Score"))
+        {
+            GameManager.PlayerType playerType = (this.transform.position.x < 0) ? GameManager.PlayerType.PlayerRight : GameManager.PlayerType.PlayerLeft;
+            OnPlayerScore?.Invoke(this, new OnPlayerScoreArgs
+            {
+                scorePlayerType = playerType
+            });
+        }
+        else if(collision.gameObject.CompareTag("Player"))
+        {
+            rb.linearVelocity *= velocityMultiplier;
+        }
     }
 }
